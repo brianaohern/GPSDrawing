9fdb46ba056e0df433201ce52d5dd8a8852402b9
zarazha <bohern@uci.edu>
2014-05-09 17:56:47 -0700

    At Least its Working
    This one works. Doesn’t do much but it works.

--- src/edu/ics/uci/gpsdrawing/GPSDrawing.java
+++ src/edu/ics/uci/gpsdrawing/GPSDrawing.java
@@ -1,29 +1,51 @@
 package edu.ics.uci.gpsdrawing;
 
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.ErrorDialogFragment;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+
+import edu.uci.ics.ics163.gpsdrawupload.StrokeManager;
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.IntentSender;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Build;
 
-public class GPSDrawing extends Activity {
+public class GPSDrawing extends Activity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
 
+	public static StrokeManager stroke_manager;
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	public LocationClient mLocationClient = new LocationClient(this, this, this);
+	public String lastLocation = "Unavailable";
+	public int stroke_color = Color.rgb(255, 255, 255);
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gpsdrawing);
 
 		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -44,6 +66,7 @@ public class GPSDrawing extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
@@ -57,22 +80,69 @@ public class GPSDrawing extends Activity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_gpsdrawing,
 					container, false);
-			
-			Button updateButton = (Button) rootView.findViewById(R.id.upload_button);
-			updateButton.setOnClickListener(new View.OnClickListener() {
-				public void onClick(View v) {
-					dynamicClick(v);
-				}
-			});
 			return rootView;
 		}
 	}
-	
-	
-	
-	public static void dynamicClick(View v)
-	{
+
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if (result.hasResolution()) {
+			try {
+				result.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+			} catch (IntentSender.SendIntentException e) {
+				e.printStackTrace();
+			}
+		} else {
+			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+			if (errorDialog != null) {
+				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+				errorFragment.setDialog(errorDialog);
+				errorFragment.show(getFragmentManager(), "Location Updates");
+			}
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		
+		Location mCurrentLocation = mLocationClient.getLastLocation();
+		lastLocation  = "(" + mCurrentLocation.getLatitude() + " , " + mCurrentLocation.getLongitude() + ")";
+		updateUI();
+	}
+
+	public void updateUI() {
+		runOnUiThread(new Runnable() {
+			public void run() {
+				TextView tv = (TextView) findViewById(R.id.location_display);
+				tv.setText(lastLocation);
+			}
+		});
 		
 	}
 
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static class ErrorDialogFragment extends DialogFragment {
+		private Dialog mDialog;
+		
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+		
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+		
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
+	}
 }
